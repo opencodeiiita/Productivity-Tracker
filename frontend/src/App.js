677f7172f59b4692972256e9b531af115d488117
Shailesh Tiwari <iib2020001@iiita.ac.in>
2021-10-05 17:06:47 +0400

    Added more components to homepage
    Issue: 1
    
    Added chart and a signup form(only frontend) to homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import Hero from './Hero';
+import Chart2 from './Chart2';
+import SignUp from './SignUp';
 
 function App() {
   return (
     <>
       <Hero />
+      <div
+        style={{
+          textAlign: 'center',
+          alignItems: 'center',
+          alignContent: 'center',
+          justifyContent: 'center',
+          maxWidth: '60%',
+          maxHeight: '100%',
+          marginLeft: '20%',
+          paddingBottom: '10vh',
+        }}
+      >
+        <Chart2 />
+      </div>
+      <SignUp />
     </>
   );
 }
---
+++ frontend/src/Chart1.js
@@ -0,0 +1,1 @@
+// https://codesandbox.io/s/chartjs-reactjs-8775k?file=/src/index.js
