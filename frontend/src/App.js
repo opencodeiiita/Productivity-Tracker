4edcc9242885e2888a9d11543c1cf9958cea4e8a
batflarrow <dwivedia2001@gmail.com>
2021-10-05 23:37:15 +0530

    Add Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Home from "./pages/home";
 import Contact from "./pages/contact";
 import Login from "./pages/login";
 import Dashboard from "./pages/dashboard";
-
+import Footer from "./component/Footer";
 function App() {
   return (
     <Router>
@@ -16,6 +16,7 @@ function App() {
         <Route path="/contact" component={Contact} />
         <Route path="/login" component={Login} />
       </Switch>
+      <Footer />
     </Router>
   );
 }
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,6 +1,18 @@
-import { Typography } from "@material-ui/core";
-
+import {
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+const useStyles = makeStyles((theme) => ({
+  homePage: {
+    height: "81.8vh"
+  }
+}));
 function Home() {
-  return <Typography>This is the homepage</Typography>;
+  const classes = useStyles();
+  return (
+    <div className={classes.homePage}>
+      <Typography>This is the homepage</Typography>
+    </div>
+  );
 }
 export default Home;
