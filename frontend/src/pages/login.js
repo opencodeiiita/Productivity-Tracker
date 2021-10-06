bf03f1f76416e4b17dffe278b9ef04e03c1559e1
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2021-10-06 22:53:11 +0530

    Added sign in with google button

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,6 +1,27 @@
-import { Typography } from "@material-ui/core";
-
+import { Typography } from '@material-ui/core';
+import Container from '@mui/material/Container';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import GoogleIcon from '../images/google.svg';
 function Login() {
-  return <Typography>Login page</Typography>;
+  return (
+    <Container align='center' sx={{ marginTop: 3 }}>
+      <Typography variant='h4' sx={{ marginTop: 3 }}>
+        Login page
+      </Typography>
+      <Button
+        variant='outlined'
+        size='large'
+        sx={{ marginTop: 3, marginBottom: 3 }}
+      >
+        <CardMedia
+          component='img'
+          image={GoogleIcon}
+          sx={{ width: 24, marginRight: 1 }}
+        />
+        <Typography variant='string'>Sign In With Google</Typography>
+      </Button>
+    </Container>
+  );
 }
 export default Login;
