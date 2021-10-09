c0b46cf05fa8ab1d9f483e93e83f85abf3216ee2
Saurav Chandawad <76427228+schandu-eng@users.noreply.github.com>
2021-10-09 13:18:23 +0530

    Updated footer

--- frontend/src/component/Footer.js
+++ frontend/src/component/Footer.js
@@ -13,11 +13,11 @@ import Instagram from '@material-ui/icons/Instagram';
 
 const useStyles = makeStyles((theme) => ({
     navbar: {
-        position: 'fixed',
+        position: 'static',
         bottom: 0,
         left: 0,
         right: 0,
-        backgroundColor: 'red',
+        backgroundColor: 'black',
     },
     navlinks: {
         marginLeft: theme.spacing(5),
