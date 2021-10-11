97f0ce7b15eef80e0ed2f480c47d8acb5fc677f8
ashutosh verma <ashutoshverma5555@gmail.com>
2021-10-12 01:26:04 +0530

    Added persistent drawer

---
+++ frontend/src/component/PersistentDrawer.js
@@ -0,0 +1,174 @@
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import { makeStyles } from '@mui/styles';
+import NotesIcon from '@mui/icons-material/Notes';
+import EventIcon from '@mui/icons-material/Event';
+import TimerIcon from '@mui/icons-material/Timer';
+import QueryStatsIcon from '@mui/icons-material/QueryStats';
+import { Tooltip } from '@mui/material';
+import DashboardGraph from './DashboardGraph';
+
+const drawerWidth = 200;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }),
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+const useStyle = makeStyles({
+   toolbar:{
+     position:'absolute',
+     top:'45px',
+    //  border:'2px solid red',
+     width:'100px',
+     height:'100px'
+     
+   }
+})
+
+export default function PersistentDrawerLeft() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+ 
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+
+  };
+  const classes = useStyle();
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      
+        <Toolbar className={classes.toolbar}>
+        <Tooltip title="Sidebar">
+        <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+         </Tooltip>
+          
+          <Typography variant="h6" noWrap component="div">
+           
+          </Typography>
+        </Toolbar>
+   
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+        <ListItem button key="notes" >
+          <ListItemIcon color='primary'>
+           <NotesIcon color='primary'></NotesIcon>
+          </ListItemIcon>
+          <ListItemText  primary="Notes" />
+        </ListItem>
+        <ListItem button key="Todolist">
+          <ListItemIcon><ListAltIcon color='primary'/></ListItemIcon>
+          <ListItemText  primary="Todo List" />
+        </ListItem>
+        <ListItem button key="reminder">
+          <ListItemIcon> <EventIcon color='primary'/></ListItemIcon>
+          <ListItemText  primary="Reminders" />
+        </ListItem>
+        <ListItem button key="Stopwatch">
+          <ListItemIcon><TimerIcon color='primary'/></ListItemIcon>
+          <ListItemText  primary="StopWatch" />
+        </ListItem>
+        <ListItem button key="Stats">
+          <ListItemIcon><QueryStatsIcon color='primary'/></ListItemIcon>
+          <ListItemText   primary="Stats" />
+        </ListItem>
+      </List>
+
+      </Drawer>
+      <Main open={open}>
+      {/* <DrawerHeader /> */}
+        <DashboardGraph></DashboardGraph>
+        
+      </Main>
+    </Box>
+  );
+}
--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -1,135 +1,14 @@
-import { Typography,  makeStyles } from "@material-ui/core";
-import Card from '@mui/material/Card';
 import React from "react";
-import {Bar} from 'react-chartjs-2';
-const state = {
-  labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-  datasets: [
-    {
-      label: 'Productive Time',
-      backgroundColor:[
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-        'rgba(155, 80, 80, 0.2)',
-      ],
+import  PersistentDrawerLeft from "../component/PersistentDrawer"
 
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-        'rgba(155, 80, 80, 1)',
-
-      ],
-      borderWidth: 2,
-      data: [18, 10, 5, 15, 12, 7, 2]
-    }
-  ]
-}
-const  test = {
-  scales: {
-    x: {
-      display: true,
-      title: {
-        display: true,
-        text: 'Days',
-        color: '#911',
-        font: {
-          family: 'Comic Sans MS',
-          size: 20,
-          weight: 'bold',
-          lineHeight: 1.2,
-        },
-        padding: {top: 5, left: 0, right: 0, bottom: 5}
-      }, 
-     
-    },
-    y: {
-      min:0,
-      max:24,
-      
-      display: true,
-      title: {
-        display: true,
-        text: 'Time(in hours)',
-        color: '#191',
-        font: {
-          family: 'Times',
-          size: 20,
-          style: 'normal',
-          lineHeight: 1.2
-        },
-        padding: {top: 5, left: 0, right: 0, bottom: 0}
-      },
-      ticks:{
-        beginAtZero: true,
-    
-        
-      }
-    },
-    
-  
-  }
-}
-const myStyle = makeStyles(
- { barDiv: {
-   width:'100vw',
-   height:'90vh',
-     display:'flex',
-     alignItems:'center',
-     justifyContent:'center',
-     backgroundColor:'white',
-     flexDirection:'column',
-    
-  },
-  barTypography:{
-    display:'flex',
-    alignItems:'center',
-    justifyContent:'center',
-    margin:'10px 10px 0px 0px',
-  },
-  dashStyle:{
-    position:'relative',
-    letterSpacing:'0',
-    display:'inline-block',
-    margin:'10px',
-  },
-  barCard:{
-    width:'45vw',
-    minWidth:'400px',
-    height:'auto'
-  }
-
-}
-)
 
 
 function Dashboard() {
-  const clases = myStyle();
+
   return(
   <>
-    <div className={clases.barDiv}>
-     
-    <Card className={clases.barCard}  >
-    <h3 className={clases.dashStyle}>Dashboard</h3>
-      <Typography component='div'className={clases.barTypography} >
-          <Bar
-            data={state}
-            width = {100}
-            height = {60}
-            options={test}
-           
-            
-          />    
-      </Typography>
-    </Card>
-    </div>
+     <PersistentDrawerLeft />
+    
    
      
   </>
