48eb663eaab553cd548b7efae5d249c4220d08df
Shailesh Tiwari <iib2020001@iiita.ac.in>
2021-10-19 02:43:54 +0400

    Added POST/note
    Added POST/note

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,11 +3,27 @@ var router = express.Router();
 
 const { getProfile } = require('../controllers/authController');
 
+const Note = require('../models/note');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({'msg' : "Hello World"});
+router.get('/', function (req, res, next) {
+    res.json({ msg: 'Hello World' });
 });
 
-router.get('/profile', getProfile)
+router.get('/profile', getProfile);
+
+router.post('/note', async (req, res) => {
+    try {
+        const note = new Note({
+            user_id: req.user._id,
+            title: req.body.title,
+            text: req.body.text,
+        });
+        const createdNote = await note.save();
+        res.send({ Note: createdNote });
+    } catch (err) {
+        res.send({ message: 'Note could not be created' });
+    }
+});
 
 module.exports = router;
