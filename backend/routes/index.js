82f211a7b93390411fe05ea7c9f2633db198826c
abhnv_pthk <coolabhinav57@gmail.com>
2021-10-18 13:02:48 +0530

    #82 POST /todo

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const { getProfile } = require('../controllers/authController');
+const ToDo = require('../models/todo');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,5 +10,19 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/profile', getProfile)
+
+router.post('/todo',async(req,res)=>{
+  try{
+    const todo = new ToDo({
+      user_id: req.user.gid,
+      description: req.body.description,
+      checked: req.body.checked,
+    })
+    const createdTodo = await todo.save();
+    res.send({Todo: createdTodo});
+  }catch(err){
+    res.send({message: "Todo not created"})
+  }
+})
 
 module.exports = router;
