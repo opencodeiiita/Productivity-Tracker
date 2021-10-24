33321b16782e809a999ba6b9faf4414498ac739f
Ksathwik03 <iit2020241@iiita.ac.in>
2021-10-24 17:49:48 +0530

    made required changes

--- frontend/src/component/Notes/Notes.jsx
+++ frontend/src/component/Notes/Notes.jsx
@@ -1,59 +1,61 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-
+import './Notes.css'
 
 export default function Notes() {
     const [Title, setTitle] = useState('')
     const [Description, setDescription] = useState('')
-
-    var notes =  []
-    useEffect(() => {
-      axios.get('http://localhost:5000/note')
-      .then(res => {
-          notes = res
-      })          
-    },)
+    const [notes, setnotes] = useState([])
+    const [time, settime] = useState('')
 
     const submit = async(e) => {
         e.preventDefault()
-        await axios.post('http://localhost:5000/note' , {body:{'title':Title,text:Description}})
-        .then(res => {
-            alert(res)
-        })
+        if(!Title || !Description){
+            return alert("Please fill title and description")
+        }
+        setnotes([...notes,{'Title': Title, 'Description': Description, 'Time': new Date().toLocaleString()}])
         setTitle('')
         setDescription('')
-        document.querySelector('.noteTitle').innerHTML = ''
-        document.querySelector('.noteDescription').innerHTML = ''
     }
+
     return (
         <div>
             <h1>Add a Note</h1>
             <form onSubmit = {submit}>
             <p>Title: </p>
+            
             <input 
             className = "noteTitle"
             type= {Text} 
             required = {true} 
+            value = {Title}
             onChange = {(e) => setTitle(e.target.value)}>
             </input>
+            <br />
+            <br />
+
+
             <p>Description: </p>
-            <input 
+            <textarea
+            cols={100}
+            rows = {10}
             className = "noteDescription"
             type= {Text} 
+            value = {Description}
             required = {true} 
-            onChange = {(e) => setDescription(e.target.value)}>
-            </input>
+            onChange = {(e) => setDescription(e.target.value)} />
+            <br /><br />
             <button onClick = {submit}>Add</button>
             </form>
+            <br />
             <h2>Notes</h2>
             <div className = "notes">
                 {notes.map(element => {
                     return(
-                    <div>
-                    <div>Title: {element.title}</div>
-                    <div>Description: {element.text}</div>
-                    <div>Time: {new Date(element.timeStamp).toJSON().slice(0,10).replace(/-/g,'/')}</div>
-                    <br></br>
+                    <div className="note" key="1">
+                    <div className="title">Title: {element.Title}</div>
+                    <div className="description">Description: {element.Description}</div>
+                    <div className="time">Time: {element.Time}</div>
                     </div>
                 )}) }
             </div>
