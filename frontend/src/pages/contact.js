c0683ddffa163e05940cba935882eff388b5ec3a
Shailesh Tiwari <iib2020001@iiita.ac.in>
2021-11-03 23:51:46 +0530

    Revert "Added reminder page"
    This reverts commit 949a59e7fec21f377194b6e0a6004bfe66d5e81f.

--- frontend/src/pages/contact.js
+++ frontend/src/pages/contact.js
@@ -1,12 +1,10 @@
 import { Typography } from "@material-ui/core";
 import Footer from '../component/Footer';
 import Navbar from '../component/Navbar';
-import SignUp from "../component/SignUp";
 function Contact() {
   return(<>
     <Navbar />
-   {/* <Typography>Contact Us page</Typography> */}
-    <SignUp/>
+   <Typography>Contact Us page</Typography>
    <Footer />
    </>);
 }
